80b81f141282ecbf94f72c2a5bfb63ce67c97eda
amiruddin <amiruddin@hitachi-ebwrox.com>
2019-08-12 12:55:28 +0800

    Updates

---
+++ precache-manifest.1ac8e30a6ae51628dbd7f71e67a93398.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8bf1e9602094b40c5b5007ea642443",
+    "url": "/nric-generator/index.html"
+  },
+  {
+    "revision": "165bcf35643bde363dfd",
+    "url": "/nric-generator/static/css/2.1ef35742.chunk.css"
+  },
+  {
+    "revision": "120b6655670c866aacc3",
+    "url": "/nric-generator/static/css/main.8bfabb06.chunk.css"
+  },
+  {
+    "revision": "165bcf35643bde363dfd",
+    "url": "/nric-generator/static/js/2.4ed54e56.chunk.js"
+  },
+  {
+    "revision": "120b6655670c866aacc3",
+    "url": "/nric-generator/static/js/main.19dacfdc.chunk.js"
+  },
+  {
+    "revision": "6b8de8f88ab4ca27bac2",
+    "url": "/nric-generator/static/js/runtime~main.7dc9ece3.js"
+  }
+]);
